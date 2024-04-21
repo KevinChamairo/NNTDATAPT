3dff918d6c049eefedbe336559ae01ed3d31bd17
KevinChamairo <kevin.chamairoriveros@gmail.com>
2024-04-20 21:19:22 -0500

    primer avance cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AutenticacionService } from './shared/servicios/autenticacion.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'NttDataPruebaTecnica';
+  constructor(private loginPrd:AutenticacionService){
+
+  }
+  public visualizarMenu(){
+    return this.loginPrd.habilitarlogeo();
+  }
 }
